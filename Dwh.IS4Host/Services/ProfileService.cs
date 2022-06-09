a319d52f5c563e0207326404307e50955b5a4718
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-06-09 18:49:42 +0300

    updated profile service

--- Dwh.IS4Host/Services/ProfileService.cs
+++ Dwh.IS4Host/Services/ProfileService.cs
@@ -1,22 +1,53 @@
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Dwh.IS4Host.Models;
 using IdentityModel;
+using IdentityServer4.Extensions;
 using IdentityServer4.Models;
 using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace Dwh.IS4Host.Services
 {
     public class ProfileService : IProfileService {
-        public ProfileService() { }
-        public Task GetProfileDataAsync(ProfileDataRequestContext context) {
+        // public ProfileService() { }
+        private readonly IUserClaimsPrincipalFactory<ApplicationUser> _claimsFactory;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileService(UserManager<ApplicationUser> userManager, IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory)
+        {
+            _userManager = userManager;
+            _claimsFactory = claimsFactory;
+        }
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context) {
             var roleClaims = context.Subject.FindAll(JwtClaimTypes.Role);
             var nameClaims = context.Subject.FindAll(JwtClaimTypes.Name);
+            var emailClaims = context.Subject.FindAll(JwtClaimTypes.Email);
             context.IssuedClaims.AddRange(roleClaims);
             context.IssuedClaims.AddRange(nameClaims);
-            return Task.CompletedTask;
+            context.IssuedClaims.AddRange(emailClaims);
+            
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            var principal = await _claimsFactory.CreateAsync(user);
+
+            var claims = principal.Claims.ToList();
+
+            // Add custom claims in token here based on user properties or any other source
+            claims.Add(new Claim("FullName", user.FullName ?? string.Empty));
+            claims.Add(new Claim("OrganizationId", user.OrganizationId.ToString() ?? string.Empty));
+
+            context.IssuedClaims = claims;
+
         }
 
-        public Task IsActiveAsync(IsActiveContext context) {
-            return Task.CompletedTask;
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            context.IsActive = user != null;
         }
     }
 }
